8034a81084a722276fc1318e558f084cce4b9f12
GauravGadre123 <ggadre56@gmail.com>
2022-04-05 22:38:26 +0530

    initial commit

---
+++ src/main/java/com/techno/AppJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.techno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJenkinsDockerApplication.class, args);
+	}
+
+}
